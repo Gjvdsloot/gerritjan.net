1874e97596634a4d14e2a7ab2fb61e992de05f56
Gerritjan van der Sloot <g.vandersloot@dairydatawarehouse.com>
2026-07-13 14:20:59 +0200

    -

--- app/app.vue
+++ app/app.vue
@@ -2,7 +2,7 @@
 const title = 'Gerritjan van der Sloot'
 const description = 'Softwareontwikkelaar met een passie voor moderne cloudoplossingen, .NET en Microsoft Azure.'
 const siteUrl = 'https://www.gerritjan.net'
-  
+
 useHead({
   meta: [
     { name: 'viewport', content: 'width=device-width, initial-scale=1' }
