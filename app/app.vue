7df7046a5fd42e4166af6547dfc44f8dc75c77fd
Gjvdsloot <g.j.van.der.sloot@student.rug.nl>
2026-07-13 09:12:40 +0200

    Update app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,4 +1,8 @@
 <script setup>
+const title = 'Gerritjan van der Sloot'
+const description = 'Softwareontwikkelaar met een passie voor moderne cloudoplossingen, .NET en Microsoft Azure.'
+const siteUrl = 'https://www.gerritjan.net'
+  
 useHead({
   meta: [
     { name: 'viewport', content: 'width=device-width, initial-scale=1' }
@@ -11,9 +15,6 @@ useHead({
     lang: 'nl'
   }
 })
-
-const title = 'Gerritjan van der Sloot'
-const description = 'Softwareontwikkelaar met een passie voor moderne cloudoplossingen, .NET en Microsoft Azure.'
 
 useSeoMeta({
   title,
